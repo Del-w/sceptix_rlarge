ce71a7b593088c32396b7e5fd1f86b31a3eb4dfe
Del-w <148423851+Del-w@users.noreply.github.com>
2024-01-10 16:28:46 +0530

    password has been hidden and stuff has been fixed

--- website/routes.py
+++ website/routes.py
@@ -1,49 +1,49 @@
-from flask import render_template , Blueprint , request, redirect, url_for
-from website import create_app
+from flask import render_template, Blueprint, request, redirect, url_for 
 
-routes = Blueprint ('route',__name__)
+routes = Blueprint('route', __name__)
 
 @routes.route('/')
 def index():
     return render_template('homepage.html')
 
-@routes.route('/personnel')
-def personnel():
-    return render_template('personnel.html')
 
-password_1 = '01001010 00101101 01010010 01000100 00101101 01001110 01001111'
-password_2 = '01010000 01010011 01010110 00101101 01010010 01000110 01000010'
 
-@routes.route('/check_password', methods=['POST'])
-def check_password():
-    entered_password = request.form.get('password')
+password_1_binary = '01001010 00101101 01010010 01000100 00101101 01001110 01001111'  
+password_2_binary = '01010000 01010011 01010110 00101101 01010010 01000110 01000010'  
 
-    if check_password_function(entered_password, password_1):
-        return redirect(url_for('route.auth'))
-    elif check_password_function(entered_password, password_2):
-        return redirect(url_for('route.unauth'))
-    else:
-        return redirect(url_for('route.invalid_password'))
 
-@routes.route('/auth')
+def check_password_function(entered_password, stored_password):
+
+    entered_password = (entered_password)
+
+    return entered_password == stored_password
+
+
+@routes.route('/personnel', methods=['GET', 'POST'])
+def Check_Password():
+    error_message = None
+
+    if request.method == 'POST':
+        entered_password = request.form.get('password_personnel','')
+
+        if check_password_function(entered_password, password_1_binary):
+            return redirect(url_for('route.auth'))
+        elif check_password_function(entered_password, password_2_binary):
+            return redirect(url_for('route.unauth'))
+        else:
+            error_message = 'Incorrect Password'
+
+    return render_template('personnel.html', error_message=error_message)
+
+
+@routes.route('/you-are-now-one-of-us')
 def auth():
     return render_template('auth.html')
 
-@routes.route('/unauth')
+@routes.route('/u-have-been-warned-do-not-return')
 def unauth():
     return render_template('unauth.html')
 
-@routes.route('/')
-def invalid_password():
-    return ('INVALID PASSWORD')
-
-def check_password_function(entered_password, stored_password):
-    return entered_password == stored_password
-
-@routes.route('/pass')
-def password():
-    return render_template('password.html')
-
-@routes.route('/goodbye')
-def goodbye():
-    return render_template('goodbye.html')
+@routes.route('/repent-for-your-sins')
+def p():
+    return render_template('p.html')
